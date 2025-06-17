0acb9064882b0f3ccb37f6452221c0bd936cb411
haruto356 <harutoharu327@gmail.com>
2025-06-17 18:14:52 +0900

    update: テスト用データとして立川市を表示するように変更

--- 101/js/101.js
+++ 101/js/101.js
@@ -1,3 +1,5 @@
+// レイアウト図45P A-1
+
 // ロードしたときに実行される
 window.onload = function(){
     initDraw101();
@@ -8,12 +10,18 @@ function initDraw101(){
     const context101 = document.getElementById("canvas101").getContext("2d");
 
     // 背景の描画
-    context101.fillStyle = '#fff';
-    context101.fillRect(0, 0, 1900, 900);
+    context101.fillStyle = "#fff";
+    context101.fillRect(0, 0, 900, 500);
 
     // 枠線の描画
     context101.lineWidth = 15;
-    context101.strokeStyle = '#2269b0'
-    strokeRoundRect(context101, 30, 30, 1840, 840, 2);
+    context101.strokeStyle = "#2269b0";
+    strokeRoundRect(context101, 20, 20, 860, 460, 2);
+
+    context101.fillStyle = "#2269b0";
+    context101.font = "200px 'Kosugi Maru'";
+    context101.textBaseline = "top";
+    context101.textAlign = "left";
+    context101.fillText("立川市", 110, 80);
 
 }
