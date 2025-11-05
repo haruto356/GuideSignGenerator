53a366c3291ab73134bcb68a2498971a587c75d9
haruto356 <harutoharu327@gmail.com>
2025-11-05 14:09:40 +0900

    update: canvasにcssを適用し、inputにidを付与

--- js/105.js
+++ js/105.js
@@ -11,5 +11,34 @@ function download(){
 
 // 地名を描画する関数
 function draw(){
+    const canvas = document.getElementById("canvas");
+    /** @type {CanvasRenderingContext2D} */
+    const context = canvas.getContext("2d");
 
+    // 日本語地名
+    const inputNameJa1 = document.getElementById("inputNameJa1").value;
+    const inputNameJa2 = document.getElementById("inputNameJa2").value;
+    const inputNameJa3 = document.getElementById("inputNameJa3").value;
+
+    // ローマ字地名
+    const inputNameEn1 = document.getElementById("inputNameEn1").value;
+    const inputNameEn2 = document.getElementById("inputNameEn2").value;
+    const inputNameEn3 = document.getElementById("inputNameEn3").value;
+
+    // 距離
+    const inputDistance1 = document.getElementById("inputDistance1").value;
+    const inputDistance2 = document.getElementById("inputDistance2").value;
+    const inputDistance3 = document.getElementById("inputDistance3").value;
+
+    // canvasサイズの設定（縦）
+    canvas.height = 500;
+    if(inputNameJa2 != ""){
+        canvas.height = 900;
+    }
+    if(inputNameJa3 != ""){
+        canvas.height = 1300;
+    }
+
+    context.fillStyle = "#ccc";
+    context.fillRect(0,0,canvas.width, canvas.height)
 }
